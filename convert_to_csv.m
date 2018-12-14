a12a37f419d185bf67d8d4846fd9f88c8716ab5f
Casey Wilson <cawilson1@g.cofc.edu>
2018-12-13 19:54:46 -0500

    convert labels and feature names to csv. read csvs in python

--- convert_to_csv.m
+++ convert_to_csv.m
@@ -4,8 +4,13 @@ allLabels = load('.\zscore_feature_sets\allLabels.mat');
 
 
 featureVals = [allFeatures.AllFeatures.CVPacketSize allFeatures.AllFeatures.ThirdMomentPacketSize allFeatures.AllFeatures.CVPacketInterarrival allFeatures.AllFeatures.ThirdMomentPacketInterarrival allFeatures.AllFeatures.CorJavaScriptCount allFeatures.AllFeatures.HTTPorFTPandExeCodeCount allFeatures.AllFeatures.HTTPandMalformedCount allFeatures.AllFeatures.FTPandCcodeCount allFeatures.AllFeatures.SYNCount allFeatures.AllFeatures.ECHOCount];
-features = ["CVPacketSize" "ThirdMomentPacketSize" "CVPacketInterarrival" "ThirdMomentPacketInterarrival" "CorJavaScriptCount" "HTTPorFTPandExeCodeCount" "HTTPandMalformedCount" "FTPandCcodeCount" "SYNCount" "ECHOCount"];
+features = ["CVPacketSize"; "ThirdMomentPacketSize"; "CVPacketInterarrival"; "ThirdMomentPacketInterarrival"; "CorJavaScriptCount"; "HTTPorFTPandExeCodeCount"; "HTTPandMalformedCount"; "FTPandCcodeCount"; "SYNCount"; "ECHOCount";];
 %B = padarray(i,1,0,'pre');
 %B(1,1) = "test";
-dlmwrite('featureVals.csv',featureVals);
-%dlmwrite('features.csv',features);
+%dlmwrite('featureVals.csv',featureVals);
+
+features = array2table(features);
+writetable(features,"features.csv");
+
+%labels = cell2table(allLabels.AllLabels.HLClass);
+%writetable(labels,"labels.csv");
---
+++ read_featureval_csv.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 19:30:51 2018
+
+@author: Casey
+"""
+
+import pandas as pd
+import numpy as np
+
+featureVals = pd.read_csv('featureVals.csv',
+                        sep=',',
+                        header=None)
+
+labels = pd.read_csv('labels.csv',
+                     sep=',',
+                     header=0)#ignore first col because matlab writes an irrelevant var name to first col
+    
+featureNames = pd.read_csv('features.csv',
+                     sep=',',
+                     header=0)#same as above
+                     
+featureValnp = np.array(featureVals,dtype="float")
+print(featureValnp)
+print(np.shape(featureValnp))
+
+#print(labels)
+print(featureNames)
