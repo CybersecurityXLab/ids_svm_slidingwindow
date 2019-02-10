c02f3eacf4389531ee12df16878a4022af55d500
Casey Wilson <cawilson1@g.cofc.edu>
2019-02-10 13:37:56 -0500

    add description to custom metric

--- custom_metric.py
+++ custom_metric.py
@@ -4,24 +4,75 @@ Created on Sat Feb  9 11:28:05 2019
 
 @author: Casey
 
+create custom metric to give score to evaluate performance for ranking in recursive feature elim
 
-create custom metric
+For this problem, standard classification metrics are inadequate. The nature of time windows presents 
+an interesting metric problem when compared to usual classification tasks. A simple one to one 
+comparison of prediction to label for each individual second does not completely capture the 
+effectiveness of attack classification when an attack is a sequence of seconds, not a single second.
+This means we not only want to maximize standard TPR, where each individual SECOND in an attack is
+considered a positive, we also want to maximize the TPR when considering each individual ATTACK as 
+a positive. 
+For a simple example where 0 is a non-attack second and 1 is an attack second:[0,0,0,1,1,1,1,0,0,0]. 
+In this example indeces 3, 4, 5, and 6 are each part of single, four-second attack. We not only want 
+each index to be considered in its own right (a perfect TPR being 4/4 correctly classified positives),
+we want the entire attack itself to be considered valueable (1/1 attacks in this 10 second interval
+correctly classified). We don't simply want one or the other, both are important to maximize. For this
+reason, we want to use both measures in our metric. These are standard TPR, and per-attack TPR. Per
+attack TPR is calculated by if even 1 second is correctly classified as positive in the attack, the 
+attack itself counts as a true positive.
+
+In addition to these two metrics, we want to reduce the false positive rate, for well-established
+reasons given in the base rate fallacy paper.
+
+Another factor is that while we do want standard TPR to have value, if it is given equal weight to FPR 
+and per-attack TPR, the standard TPR can overwhelm the score of the metric and give a poor score to an 
+IDS that in reality performs very well with regards to total attacks detected and FPR.
+For example, consider 1000 seconds, with 5 attacks, each 50 seconds. If the classifier has zero FPs
+and correctly classifies 20/50 seconds in each attack as a positive, while in reality this would be
+a very effective IDS, the score would be ((20*5/50*5) * 1.0 * 1.0) = 0.4, even though every attack
+was classified as such, there were no false positives, and a large percentage of the individual 
+seconds were classified as an attack relative to the number of false positives. 
+For this reason, the standard TPR is weighted to only have 1/4 the importance of the other two measures.
+This way, it still is considered, but it is not as important as actually detecting attacks or reducing
+false positives. In an intuitive sense, a few missed time windows don't penalize the score all that much,
+which is good, because they might not really matter.
+
+Since we still want the score to be between 0 for the worst and 1 for a perfect prediction, the standard
+TPR is weighted in a way that allows the scores to stay in this range with these attributes. This means
+that instead of pure TPR, we want to reduce the penalization of this measure, while still keeping the
+best and worse case of the TPR between 0 and 1. This is done by creating a functiono f the TPR, shown
+below.   
+f(tpr) = tpr / (tpr + (0.25*(1-tpr)))  #gives tpr 1/4 weight but still allows customMetric to be between 0 and 1
+
+
+The final formula for the metric is given below:
+    
+customMetric = f(tpr) * tnr * perAttackTPR
+0   <=   customMetric   <=   1
+
+
+Adjustments should be made to the formula in the case of all 0s or all 1s in the correctly labelled
+data. In these cases, simply remove the metrics where the denomintors would be 0.
 """
 from sklearn.metrics import confusion_matrix
 
 
 def main():
     actualY = [0,0,0,0,1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0]
+    perfectY = actualY#everything predicted correct, should be 1, a perfect score
     
-    allPosY = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-    allNegY = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    perfectY = actualY
+    allPosY = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]#all predicted positive. Useless. should be 0
+    allNegY = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#all predicted negative. Useless. should be 0
     sampleYBadThreshold = [0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]#perfect attack WINDOW percent (there could be multiple overlapping attacks), perfect rule 2, each attack only has 1 correctly positively classified second
     sampleYBadFPs = [0,1,0,1,1,0,0,0,1,1,1,0,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,0,1,1,0,1,0,0,0,0,1,0,0,1,1,0,0,0,0,1,1,0]#perfect attack percent, bad FP, good threshold
     sampleYBadAttackPercent = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#missed half attacks, perfect fp, perfect threshold
     sampleYBadFPandThresh = [0,1,1,0,1,0,0,1,1,0,0,0,0,0,1,1,0,0,1,0,1,0,1,1,0,1,0,0,0,0,0,1,1,0,1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0]#perfect attacks, bad fp, bad threshold
+    randomY = [1,1,0,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,0,0,1,1,1,1,1,1,0,1,1,0,0,0,0,1,1,0,0,1,1,1,0,1,1,1,0,0,0,1]
+    sampleYBadThresholdQuarter = [0,0,0,0,1,0,0,0,1,1,1,0,1,1,0,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0]
+
     
-    chosenList = sampleYBadThreshold
+    chosenList = sampleYBadThresholdQuarter
     
     getCustomMetric(actualY,chosenList)
 
@@ -38,6 +89,8 @@ def main():
 #when verifying, jump to index 34 of the predicted. If any of the predicted value at index == any of the actual value at index, increment 1 to total attack windows correctly classified. break current window loop
 #after this, find the percentage of attacks classified with at least 1 window as an attack.
 
+
+#eventually check to make sure all actualY are not the same (i.e. all 0s or all 1s). This will give nan, in the case of zero, use regular accuracy as the metric, which in this case is essentially the same thing as the TNR. In the case of all 1s, use custom metric without TNR. This because the FPR would have a denominator of 0, and would not be a relevant metric.
 def getCustomMetric(actualY,predictedY):
     
     attackIndexList = []
@@ -74,9 +127,6 @@ def getCustomMetric(actualY,predictedY):
     fpr = falsePositives/totalNegatives
     tnr = 1 - fpr
     
-    #print(truePositives/totalPositives)#TPR
-    #fpr = falsePositives/totalNegatives#FPR
-    #print(1-fpr)#TNR
     totalAttacksCounted = 0
     
     for i in range(len(attackIndexList)):#for every attack
@@ -91,6 +141,8 @@ def getCustomMetric(actualY,predictedY):
     perAttackTPR = totalAttacksCounted / len(attackIndexList)
     print(perAttackTPR)
     
+    #we want to use the TPR, but we only want to give it 1/4 weight. Getting every second classified in a window is not as important as classifying some of the correct seconds or reducing false positive rates
+    # f(tpr) gives the value only 1/4 weight, but still keeps the final score between 0 (worst) and 1 (best)
     fOfTpr = tpr / (tpr + (0.25*(1-tpr)))
     print('weighted tpr', fOfTpr)
     
@@ -101,24 +153,7 @@ def getCustomMetric(actualY,predictedY):
     
     customMetric = fOfTpr * tnr * perAttackTPR
     
-    print('\ntpr:',tpr,"\ntnr:",tnr,'\nperAttackTPR:',perAttackTPR,'\n\nCustom Metric Score:', customMetric)
+    print('pure tpr:',tpr, '\n\nweighted tpr:', fOfTpr, "\ntnr:",tnr,'\nperAttackTPR:',perAttackTPR,'\n\nCustom Metric Score:', customMetric)
     
-
-'''
-#total attacks
-
-countSwitch = False
-prevState = False
-
-totalAttacks = 0
-
-
-for idx in range(len(actualY)-1):#count the number of times the label changes# for every other change, increment the attack
-    if actualY[idx] == actualY[idx + 1]:#was changed. Later divide by 2 since this is binary
-        totalAttacks += 1
-    
-if actualY[0] == 1:#if the first element is 1, zeros are counted as the 'attack'. Subtract 1 from total count
-        
-'''
 
 main()
